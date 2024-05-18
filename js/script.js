800af769707d36a99b6f7d7e2a332b5035aa8d9f
Mosab Ahmed Irwished <42383497+Mosab97@users.noreply.github.com>
2024-05-18 21:35:31 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,198 @@
+(function($) {
+	
+	"use strict";
+	
+	//Hide Loading Box (Preloader)
+	function handlePreloader() {
+		if($('.preloader').length){
+			$('.preloader').delay(200).fadeOut(500);
+		}
+	}
+	
+	//Update Header Style and Scroll to Top
+	function headerStyle() {
+		if($('.main-header').length){
+			var windowpos = $(window).scrollTop();
+			var siteHeader = $('.main-header');
+			var scrollLink = $('.scroll-top');
+			if (windowpos >= 110) {
+				siteHeader.addClass('fixed-header');
+				scrollLink.addClass('open');
+			} else {
+				siteHeader.removeClass('fixed-header');
+				scrollLink.removeClass('open');
+			}
+		}
+	}
+	
+	headerStyle();
+
+
+	//Submenu Dropdown Toggle
+	if($('.main-header li.dropdown ul').length){
+		$('.main-header .navigation li.dropdown').append('<div class="dropdown-btn"><span class="fa fa-angle-right"></span></div>');
+		
+	}
+
+	//Mobile Nav Hide Show
+	if($('.mobile-menu').length){
+		
+		$('.mobile-menu .menu-box').mCustomScrollbar();
+		
+		var mobileMenuContent = $('.main-header .menu-area .main-menu').html();
+		$('.mobile-menu .menu-box .menu-outer').append(mobileMenuContent);
+		$('.sticky-header .main-menu').append(mobileMenuContent);
+		
+		//Dropdown Button
+		$('.mobile-menu li.dropdown .dropdown-btn').on('click', function() {
+			$(this).toggleClass('open');
+			$(this).prev('ul').slideToggle(500);
+		});
+		//Menu Toggle Btn
+		$('.mobile-nav-toggler').on('click', function() {
+			$('body').addClass('mobile-menu-visible');
+		});
+
+		//Menu Toggle Btn
+		$('.mobile-menu .menu-backdrop,.mobile-menu .close-btn').on('click', function() {
+			$('body').removeClass('mobile-menu-visible');
+		});
+	}
+	$(function () {
+		$('[data-toggle="tooltip"]').tooltip()
+	})
+
+	$(".links-widget a[href^='#']").on("click", function(e) {
+		$("html, body").animate({
+			scrollTop: $($(this).attr("href")).offset().top
+		}, 1000);
+		return false;
+	});
+
+
+	$(".main-menu a[href^='#']").on("click", function(e) {
+		$(this).addClass('active');
+		$("html, body").animate({
+			scrollTop: $($(this).attr("href")).offset().top-100
+		}, 1000);
+		return false;
+	});
+
+	// Scroll to a Specific Div
+	if($('.scroll-to-target').length){
+		$(".scroll-to-target").on('click', function() {
+			var target = $(this).attr('data-target');
+		   // animate
+		   $('html, body').animate({
+			   scrollTop: $(target).offset().top
+			 }, 1000);
+
+		});
+	}
+
+	// Elements Animation
+	if($('.wow').length){
+		var wow = new WOW({
+		mobile:       false
+		});
+		wow.init();
+	}
+
+
+
+	if ($('.brands-carousel').length) {
+		$('.brands-carousel').owlCarousel({
+			animateOut: 'fadeOut',
+			animateIn: 'fadeIn',
+			items:3,
+			loop:true,
+			margin:10,
+			dots:false,
+			nav:false,
+			smartSpeed: 300,
+			autoplay: 3000,
+			responsive:{
+				0:{
+					items:2
+				},
+				600:{
+					items:3
+				},
+				1000:{
+					items:3
+				}
+			}
+
+		});
+	}
+
+	//Accordion Box
+	if($('.accordion-box').length){
+		$(".accordion-box").on('click', '.acc-btn', function() {
+			
+			var outerBox = $(this).parents('.accordion-box');
+			var target = $(this).parents('.accordion');
+			
+			if($(this).hasClass('active')!==true){
+				$(outerBox).find('.accordion .acc-btn').removeClass('active');
+			}else{
+				$(outerBox).find('.accordion').children('.acc-content').slideUp(300);
+				$(outerBox).find('.accordion .acc-btn').removeClass('active');
+
+			}
+			
+			if ($(this).next('.acc-content').is(':visible')){
+				return false;
+			}else{
+				$(this).addClass('active');
+				$(outerBox).children('.accordion').removeClass('active-block');
+				$(outerBox).find('.accordion').children('.acc-content').slideUp(300);
+				target.addClass('active-block');
+				$(this).next('.acc-content').slideDown(300);	
+			}
+		});	
+	}
+
+
+
+
+	if($('.paroller').length){
+		$('.paroller').paroller({
+			  factor: 0.05,            // multiplier for scrolling speed and offset, +- values for direction control  
+			  factorLg: 0.05,          // multiplier for scrolling speed and offset if window width is less than 1200px, +- values for direction control  
+			  type: 'foreground',     // background, foreground  
+			  direction: 'horizontal' // vertical, horizontal  
+		});
+	}
+
+
+
+	/*	=========================================================================
+	When document is Scrollig, do
+	========================================================================== */
+
+
+
+	/* ==========================================================================
+   When document is Scrollig, do
+   ========================================================================== */
+	
+	$(window).on('scroll', function() {
+		headerStyle();
+	});
+
+	
+	
+	/* ==========================================================================
+   When document is loaded, do
+   ========================================================================== */
+	
+	$(window).on('load', function() {
+		handlePreloader();
+		enableMasonry();
+		expertizeRoundCircle ();
+	});
+
+	
+
+})(window.jQuery);
